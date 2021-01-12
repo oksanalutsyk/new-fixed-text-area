5eed7030e384d28c63e191ea304924876d35f62c
oksanalutsyk <oksanalutsyk95@gmail.com>
2021-01-12 13:38:58 +0200

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { combineLatest, fromEvent } from 'rxjs';
+import { combineLatest, fromEvent, Subscription } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
@@ -13,31 +13,63 @@ export class AppComponent implements OnInit {
   @ViewChild('lineHeightInput', { static: true }) lineHeightInput: ElementRef;
   @ViewChild('fontSizeInput', { static: true }) fontSizeInput: ElementRef;
 
+  private subscription = new Subscription();
+
   rowsCount = '5';
-  lineHeight = '50px';
+  lineHeight = '20px';
   fontSize = '20px';
 
   ngOnInit(): void {
+    this.initComponent();
+  }
+
+  private initComponent() {
     // const textArea$ = fromEvent(this.textArea.nativeElement, 'input');
-    const rowsCountInput$ = fromEvent(this.rowsCountInput.nativeElement, 'input');
-    const lineHeightInput$ = fromEvent(this.lineHeightInput.nativeElement, 'input');
+    const rowsCountInput$ = fromEvent(
+      this.rowsCountInput.nativeElement,
+      'input'
+    );
+    const lineHeightInput$ = fromEvent(
+      this.lineHeightInput.nativeElement,
+      'input'
+    );
     const fontSizeInput$ = fromEvent(this.fontSizeInput.nativeElement, 'input');
-    const updateTextAreaStream$ = combineLatest([rowsCountInput$, lineHeightInput$, fontSizeInput$])
-    .pipe(
-      debounceTime(1200),
-      distinctUntilChanged()
-    )
-    .subscribe((data:any) => {
-      this.rowsCount = data[0].target.value;
-      this.lineHeight = data[1].target.value + 'px';
-      this.fontSize = data[2].target.value + 'px';
-      // data[0].target.value
-      console.log(data[0].target.value)
-    });
-      // .subscribe((data) => {
-      //   data.forEach((d:any)=> {
-      //     // console.log(d.target.value)
-      //   }) 
-      // });
+
+    const rowsCountStream$ = rowsCountInput$
+      .pipe(debounceTime(1200), distinctUntilChanged())
+      .subscribe((data: any) => {
+        this.rowsCount = data.target.value;
+      });
+    this.subscription.add(rowsCountStream$);
+
+    const lineHeightStream$ = lineHeightInput$
+      .pipe(debounceTime(1200), distinctUntilChanged())
+      .subscribe((data: any) => {
+        this.lineHeight = data.target.value + 'px';
+      });
+    this.subscription.add(lineHeightStream$);
+
+    const fontSizeStream$ = fontSizeInput$
+      .pipe(debounceTime(1200), distinctUntilChanged())
+      .subscribe((data: any) => {
+        this.fontSize = data.target.value + 'px';
+      });
+    this.subscription.add(fontSizeStream$);
+
+    // const updateTextAreaStream$ = combineLatest([
+    //   rowsCountInput$,
+    //   lineHeightInput$,
+    //   fontSizeInput$,
+    // ]).pipe(debounceTime(1200), distinctUntilChanged())
+    //   .subscribe((data: any) => {
+    //     this.rowsCount = data[0].target.value;
+    //     this.lineHeight = data[1].target.value;
+    //     this.fontSize = data[2].target.value;
+    //   });
+    //   this.subscription.add(updateTextAreaStream$);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
